fae467afd87d1a6e9e2fc37db844481943441080
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-03-17 12:09:56 +0900

    endl객체로 개행

--- G2_1/C++_기본/기본.cpp
+++ G2_1/C++_기본/기본.cpp
@@ -1,10 +1,11 @@
-// cout 객체를 가지고 있음
+// cout, endl 객체를 가지고 있음
 #include <iostream>
 
 int main(void) {
 
-	//printf >> cout
-	std::cout<< "안녕 세상아\n";
+	//printf >> cout 객체
+	// \n >> endl 객체 ? 개행
+	std::cout<< "안녕 세상아" << std::endl;
 
 	return 0;
 }
