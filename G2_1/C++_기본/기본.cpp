8eebb08ef486bcab5c105c13109686b864f6cf97
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-03-17 12:12:19 +0900

    std:: 안 써도 됨

--- G2_1/C++_기본/기본.cpp
+++ G2_1/C++_기본/기본.cpp
@@ -1,11 +1,13 @@
 // cout, endl 객체를 가지고 있음
 #include <iostream>
 
+using namespace std; // std:: 안 써도 됨
+
 int main(void) {
 
 	//printf >> cout 객체
 	// \n >> endl 객체 ? 개행
-	std::cout<< "안녕 세상아" << std::endl;
+	cout << "안녕 세상아" << endl;
 
 	return 0;
 }
