d709a81621c41ae73b042edf0ad60ee0cf266ba3
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-04-07 12:06:46 +0900

    string 문자열 시작주소

---
+++ G2_1/C++_기본/02-10 string 문자열 시작주소.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+
+#include<string.h>   //strlen()를 가지고 있음
+#include<string>   //string 클래스를 가지고 있음
+
+using namespace std;
+
+int main() {
+	char cI[10] = "aaa";
+	printf("%s \n", cI);			// 문자열의 시작주소	
+
+	string cppI = "aaa";
+	printf("%s \n", cppI.c_str());	// 문자열의 시작주소
+	
+
+	return 0;
+}
