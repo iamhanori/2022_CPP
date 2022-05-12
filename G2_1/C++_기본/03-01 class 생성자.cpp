f07096a652b1438f5e023531e35b3e8c87a7e0ca
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-05-12 12:15:47 +0900

    소멸자 추가

--- G2_1/C++_기본/03-01 class 생성자.cpp
+++ G2_1/C++_기본/03-01 class 생성자.cpp
@@ -12,6 +12,8 @@ private:
 public:
 	// 생성자 : 객체가 생성될 때, 호출되는 함수
 	Student(int Hakbun, const char* Name);
+	// 소멸자 : 객체가 메모리에서 해제될 때, 호출되는 함수
+	~Student();
 
 	void show(void);
 };
@@ -34,6 +36,12 @@ Student::Student(int Hakbun, const char* Name)
 	sName = new char[len];		// 그 크기만큼 배열로 동적할당
 	strcpy(sName, Name);		// 매개변수에 있는 문자열 복사
 };
+
+Student::~Student()
+{
+	delete []sName;
+	cout << "소멸자 호출" << endl;
+}
 
 void Student::show(void) {
 	cout << "학번은 " << nHakbun << "입니다." << endl;
