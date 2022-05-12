90d5134ee66ed901f11762cca4008bce31e852be
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-05-12 10:40:02 +0900

    0512

---
+++ G2_1/C++_기본/03-01 class 생성자.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+private:
+	int nHakbun;
+	string sName;
+
+public:
+	// 생성자 : 객체가 생성될 때, 호출되는 함수
+	Student(void);
+	Student(int Hakbun, string Name);
+
+	void show(void);
+};
+
+int main(void) {
+	Student stu1;
+	Student stu2 = Student();
+	stu1.show();
+	stu2.show();
+
+	Student stu3 = { 1111, "JWP" };
+	stu3.show();
+
+	return 0;
+}
+
+Student::Student(void) {
+	this->nHakbun = 1234;
+	this->sName = "이사랑";
+	cout << "학번이 등록되었습니다." << endl;
+}
+
+Student::Student(int Hakbun, string Name)
+// 객체 생성과 동시에 멤버변수 초기화   // 멤버변수(매개변수)
+// const/참조형 멤버변수를 사용할 수 있다.
+	: nHakbun(Hakbun), sName(Name)
+{
+	this->nHakbun = Hakbun;
+	this->sName = Name;
+	cout << "학번이 등록되었습니다." << endl;
+};
+
+void Student::show(void) {
+	cout << "학번은 " << nHakbun << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;;
+}
